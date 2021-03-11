0442824064453e9fada7e401a954c56f30c28742
Venkat <you@example.com>
2021-03-11 07:35:43 -0500

    getters in lwc

--- lwcLearning/force-app/main/default/lwc/helloWorld/helloWorld.js
+++ lwcLearning/force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,7 +1,9 @@
 import { LightningElement ,track} from 'lwc';
 
 export default class HelloWorld extends LightningElement {
-   
+    name =' Defult Name';
+    num1 = 10;
+    num2 = 20;
 
     @track address ={
         pincode: 500059,
@@ -9,8 +11,23 @@ export default class HelloWorld extends LightningElement {
         country: "India",
     }
 
-    changeHandler(event){
-        this.address.city= event.target.value;
+    users = ['John','Mark','Luke'];
+
+    get sumOfNumbers(){
+        return this.num1+this.num2;
+    }
+
+    get firstUser(){
+        return this.users[0].toUpperCase();
+    }
+
+    twoWayBindHandler(event){
+        this.name= event.target.value;
+    }
+    
+
+    trackHandler(event){
+        this.address.city = event.target.value;
     }
 
     /*
