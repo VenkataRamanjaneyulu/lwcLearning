0738005526dae17b09d4621f6d3bcb17315af744
Venkat <you@example.com>
2021-03-13 08:16:24 -0500

    Final Version After Template Looping

--- lwcLearning/force-app/main/default/lwc/templateLooping/templateLooping.js
+++ lwcLearning/force-app/main/default/lwc/templateLooping/templateLooping.js
@@ -1,5 +1,29 @@
 import { LightningElement } from 'lwc';
 
 export default class TemplateLooping extends LightningElement {
-    cars = ["Audi","BMW","Hyundai","Kia"];
+    cars = ["Audi","BMW","Hyundai"];
+
+    ceoList = [
+        {
+            name:'SundarPichai',
+            company:'Google',
+            id:1,
+        },
+        {
+            name:'MarcBeniof',
+            company:'Salesforce',
+            id:2,
+        },
+        {
+            name:'Amazon',
+            company:'JeffBezos',
+            id:3,
+        },
+        {
+            name:'Facebook',
+            company:'MarkZuckerberg',
+            id:4,
+        },
+    ];
+
 }
